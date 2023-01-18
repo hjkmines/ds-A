1c9544cb6c0bee70e2d48a9a66d232f9c0e6a4ae
e145278 <tony.kim@wnco.com>
2023-01-18 08:38:06 -0600

    test

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -76,3 +76,86 @@ const someFunction = (name) => {
 }
 
 console.log(someFunction(name1))
+
+for loops
+
+const arr3 = [1, 2, 3, 4, 5]
+const string2 = 'Maria'
+// for statement
+for (let i = 0; i < arr3.length; i++) {
+    // console.log(arr3[i])
+}
+
+for (let i = arr3.length - 1; i >= 0; i--) {
+    // console.log(arr3[i])
+}
+
+// loop through the string
+for (let i = string2.length - 1; i >= 0; i--) {
+    console.log(string2[i])
+}
+
+// while 
+const colors = ['white', 'black', 'grey', 'yellow']
+let isCorrectColor = false; 
+
+let i = 0
+while (!isCorrectColor) {
+    if (colors[i] === 'grey') {
+        isCorrectColor = true; 
+    } else {
+        i++
+    }
+}
+
+const name5 = 'Maria'
+let isCorrectLetter = false; 
+let j = 0; 
+while (!isCorrectLetter) {
+    if (name5[i] === 'r') {
+        isCorrectLetter = true; 
+    } else {
+        j++
+    }
+}
+
+// let, var, const 
+
+var name = 'Maria'
+console.log(name2)
+if (name) {
+    console.log(name)
+} else {
+    var name2 = 'Bob'
+}
+
+// for..of statement
+// array
+let nums = [1, 2, 3, 4, 5]
+for (let num of nums) {
+    // console.log(num)
+}
+
+// string
+let word = 'Tony' 
+for (let letter of word) {
+    // console.log(letter)
+}
+
+// for..in statement
+
+const profile = {
+    name: 'tony', 
+    age: 28, 
+    address: 'some address'
+}
+
+for (let key in profile) {
+    console.log(profile[key])
+}
+
+console.log(profile.name)
+
+// profile['name'] ---> 'tony'
+// profile['age'] ---> 28
+// profile['address'] ---> 'some address
