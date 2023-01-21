4206071cd90dcadd49419b92288cf8737e20b905
e145278 <tony.kim@wnco.com>
2023-01-21 15:02:05 -0600

    test

--- bst.js
+++ bst.js
@@ -95,6 +95,64 @@ class BinarySearchTree {
         }
         return false;
     }
+    BFS() {
+        let node = this.root; 
+        let data = []; 
+        let queue = []; 
+
+        queue.push(node); 
+
+        while (queue.length) {
+            node = queue.shift(); 
+            data.push(node.value); 
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+
+        return data;
+    }
+
+    DFSPreOrder() {
+        let data = []; 
+
+        function traverse(node) {
+            data.push(node.value)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+
+        traverse(this.root); 
+
+        return data
+    }
+    DFSPostOrder() {
+        let data = []; 
+
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+
+        traverse(this.root); 
+
+        return data
+    }
+
+    DFSInOrder() {
+        let data = []; 
+
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            data.push(node.value)
+            if (node.right) traverse(node.right)
+        }
+
+        traverse(this.root); 
+
+        return data
+    }
 }
 
 
