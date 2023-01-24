39c7604dc9242b24f1e1e91f26bf0e5c1309119d
e145278 <tony.kim@wnco.com>
2023-01-23 18:24:22 -0600

    ts

--- sll.js
+++ sll.js
@@ -1,6 +1,33 @@
 class Node {
-    constructor(val) {
+    constructor(value) {
         this.value = value; 
         this.next = null; 
     }
 }
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0; 
+    }
+
+    push(value) {
+        let newNode = new Node(value); 
+
+        if (!this.head) {
+            this.head = newNode; 
+            this.tail = newNodej; 
+        } else {
+            this.tail.next = newNode; 
+            this.tail = newNode; 
+        }
+
+        this.length++; 
+        return this
+    }
+}
+
+let newLinkedList = new SinglyLinkedList()
+
+newLinkedList.push(9)
