063b168dbd4997f2f3e34ce48f35e34c17fc0f42
e145278 <tony.kim@wnco.com>
2023-02-11 15:11:02 -0600

    tes

--- sll.js
+++ sll.js
@@ -106,4 +106,44 @@ class SinglyLinkedList {
 
         return this; 
     }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null; 
+        let counter = 0; 
+        let current = this.head; 
+
+        while (counter !== index) {
+            current = current.next; 
+            counter++
+        }
+
+        return current; 
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index); 
+
+        if (foundNode) {
+            foundNode.val = val; 
+            return true;
+        } else {
+            return false; 
+        }
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false; 
+        if (index === this.length) return this.push(val); 
+        if (index === 0) return this.unshift(val); 
+
+        let newNode = new Node(val)
+        let prev = this.get(index - 1)
+        let temp = prev.next; 
+
+        prev.next = newNode; 
+        newNode.next = temp
+
+        this.length++
+        return this; 
+    }
 }
