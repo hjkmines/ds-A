1e11a2b32fcaa71e8fcee193e7f8224c25a48e51
e145278 <tony.kim@wnco.com>
2023-02-16 12:46:01 -0600

    test

--- binary_heaps.js
+++ binary_heaps.js
@@ -25,4 +25,54 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+
+    extractMax() {
+        const max = this.values[0]; 
+        
+        const end = this.values.pop()
+
+        if (this.values.length) {
+            this.values[0] = end; 
+            this.sinkDown()
+        }
+
+        return max; 
+    }
+
+    sinkDown() {
+        let idx = 0; 
+        const length = this.values.length; 
+        const element = this.values[0]
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1; 
+            let rightChildIdx = 2 * idx + 2; 
+
+            let leftChild, rightChild
+
+            let swap = null; 
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx]; 
+                if (leftChild > element) swap = leftChildIdx
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx]
+                if (
+                    (!swap && rightChild > element) ||
+                    (swap && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx
+                }
+            }
+
+            if (!swap) break; 
+
+            this.values[idx] = this.values[swap]
+            this.values[swap] = element; 
+
+            idx = swap
+        }
+    }
 }
