39c73c32539898ce43fb6ecc5f79d0173c871f80
Maria Delacruz <mar23iaa@gmail.com>
2023-02-24 05:31:09 -0800

    added a controller, routes for my user and made a connection to the server.js file

--- node/server.js
+++ node/server.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const dotenv = require('dotenv');
 const category = require('./routes/category');
 const morgan = require('morgan')
@@ -7,7 +7,7 @@ const item = require('./routes/item');
 
 dotenv.config({ path: './config/config.env' })
 
-const app = express(); 
+const app = express();
 
 if (process.env.NODE_ENV = 'dev') {
     app.use(morgan('dev'))
@@ -19,8 +19,9 @@ app.use(bodyParser.json())
 // hook up our resources
 app.use('/category', category)
 app.use('/item', item)
+app.use('/user', user)
 
-const PORT = process.env.PORT || 5001; 
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
