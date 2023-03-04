e7752f65b3cd109cf4f8357c9e81840c007d5fae
e145278 <tony.kim@wnco.com>
2023-03-04 14:25:20 -0600

    test

--- trees.js
+++ trees.js
@@ -76,7 +76,35 @@ class BinarySearchTree {
 
         return false; 
     }
-}
+
+    BFS() {
+        let node = this.root; 
+        let visited = []; 
+        let queue = [node]; 
+
+        while (queue.length) {
+            node = queue.shift(); 
+            visited.push(node.value); 
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+
+        return visited;
+    }
+
+    DFSPreOrder() {
+        let visited = []; 
+        
+        function traverse(node) {
+            visited.push(node.value)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return visited;
+    }
+ }
 
 let tree = new BinarySearchTree(); 
 
