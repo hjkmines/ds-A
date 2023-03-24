9cf7d70c2c22c3b451262e82eb8b58d620f831ee
e145278 <tony.kim@wnco.com>
2023-03-24 07:09:57 -0500

    test

--- phase1/js_challenges.js
+++ phase1/js_challenges.js
@@ -34,4 +34,48 @@ const oddOrEven = (str) => {
 
 oddOrEven('Maria')
 
-//
+// Fizz Buzz
+
+// if odd, "fizz", 
+// if even , "buzz", 
+// if number is dividible by 3 and 5, "fizzbuzz"
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+const fizzBuzz = (arr) => {
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 === 0) {
+            console.log('fizz');
+        } else if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log('fizzbuzz')
+        }
+    }
+}
+
+
+// create a function that returns a integer in reverse javascript
+// 5683 ---> 3865
+function reverseInt(int) {
+    const test = int.toString().split('').reverse().join('')
+    const realNumber = parseInt(test)
+    console.log(typeof realNumber);
+    return realNumber
+}
+reverseInt(5683)
+
+// filter out even number
+const arr1 = [1, 3, 5, 8, 9] 
+const filtered = arr1.filter(num => num % 2 === 1)
+
+console.log(filtered)
+
+function capitalizeLetters(str) {
+    const capitalized = str.toLowerCase().split(' ')
+    for (i = 0; i < capitalized.length; i++) {
+        capitalized[i] = capitalized[i].charAt(0).toUpperCase() + capitalized[i].substring(1);
+    }
+    return capitalized.join(' ')
+}
+capitalizeLetters('i love javascript')
