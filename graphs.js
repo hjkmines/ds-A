14db8368f6fa17dc92f444886956ba2d16ede793
e145278 <tony.kim@wnco.com>
2023-03-25 14:18:16 -0500

    test

--- graphs.js
+++ graphs.js
@@ -26,6 +26,80 @@ class Graph {
 
         delete this.adjList[vertex]
     }
+
+    depthFirstRecursive(start) {
+        const result = []; 
+
+        const visited = {}; 
+        const adjList = this.adjList; 
+
+        (function dfs(vertex) {
+            if (!vertex) return null; 
+            visited[vertex] = true; 
+            result.push(vertex); 
+
+            adjList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) return dfs(neighbor)
+            })
+        })(start)
+
+        return result;
+    }
+
+    depthFirstIterative(start) {
+        const stack = [start]
+
+        const result = []; 
+
+        const visited = {}; 
+
+        const adjList = this.adjList;
+
+        let currentVertex; 
+        visited[start] = true; 
+
+        while (stack.length) {
+            currentVertex = stack.pop(); 
+            result.push(currentVertex)
+            
+            adjList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[currentVertex] = true; 
+                    
+                    stack.push(neighbor)
+                }
+            })
+        }
+
+        return result;
+    }
+
+    breadthFirst(start) {
+        const queue = [start]
+
+        const result = []
+
+        const visited = {}
+
+        const adjList = this.adjList; 
+
+        let currentVertex; 
+
+        visited[start] = true; 
+        while (queue.length) {
+            currentVertex = queue.shift()
+            result.push(currentVertex); 
+
+            adjList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true
+                    queue.push(neighbor)
+                }
+            })
+        }
+
+        return result
+    }
 }
 
 const g = new Graph(); 
